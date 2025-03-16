96d79eab9673ebbdaca48dcb81cae7c56864997f
Hongyu60 <benoit.galek@gmail.com>
2025-03-16 10:58:59 +0800

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+# Écrivez votre nom = "Benoit"
+Age = "20"
+print(f"Je m'appelle {nom} et j'ai {Age} ans.")
+Age = "35"
+print(f"Je m'appelle {nom} et j'ai {Age} ans maintenant")
+code ici !
