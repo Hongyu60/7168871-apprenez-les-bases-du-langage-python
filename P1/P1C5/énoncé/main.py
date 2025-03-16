804f39dbf60a54d2558d658788cacf18af783af0
Hongyu60 <benoit.galek@gmail.com>
2025-03-16 12:32:01 +0800

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+nom = "Benoit"
+age = "35"
+taille = "1,82"
+est_etudiant = False
+
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est étudiant: {est_etudiant}")
+
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
