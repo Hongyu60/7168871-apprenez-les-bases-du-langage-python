33db5a80d0ff862a4c17596706e3fca3627e1562
Hongyu60 <benoit.galek@gmail.com>
2025-03-17 12:10:13 +0800

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,10 @@
-# Écrivez votre code ici !
+fruits = {"pomme": "rouge", "banane": "jaune", "orange": "orange"}
+print(fruits)
+fruits["kiwi"] = "vert"
+print(fruits)
+couleur_banane = fruits["banane"]
+print(couleur_banane)
+fruits["pomme"] = "vert"
+print(fruits)
+del fruits["orange"]
+print(fruits.keys())# Écrivez votre code ici !
